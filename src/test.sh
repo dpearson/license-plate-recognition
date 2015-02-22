ec26f3c399fc14207d859d4819900c5ab17cea5b
David Pearson <dpearson26@gmail.com>
2015-02-21 20:25:28 -0500

    Add the testing script

---
+++ src/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+../bin/recognize ../train_data/test_images/image_0002.jpg
+../bin/recognize ../train_data/test_images/image_0009.jpg
+../bin/recognize ../train_data/test_images/image_0019.jpg
+../bin/recognize ../train_data/test_images/image_0022.jpg
+../bin/recognize ../train_data/test_images/image_0032.jpg
+../bin/recognize ../train_data/test_images/image_0041.jpg
+../bin/recognize ../train_data/test_images/image_0042.jpg
+../bin/recognize ../train_data/test_images/image_0045.jpg
+../bin/recognize ../train_data/test_images/image_0051.jpg
+../bin/recognize ../train_data/test_images/image_0056.jpg
+../bin/recognize ../train_data/test_images/image_0062.jpg
+../bin/recognize ../train_data/test_images/image_0063.jpg
+../bin/recognize ../train_data/test_images/image_0069.jpg
+../bin/recognize ../train_data/test_images/image_0070.jpg
+../bin/recognize ../train_data/test_images/image_0074.jpg
+../bin/recognize ../train_data/test_images/image_0079.jpg
+../bin/recognize ../train_data/test_images/image_0083.jpg
+../bin/recognize ../train_data/test_images/image_0088.jpg
+../bin/recognize ../train_data/test_images/image_0101.jpg
+../bin/recognize ../train_data/test_images/image_0102.jpg
+../bin/recognize ../train_data/test_images/image_0109.jpg
+../bin/recognize ../train_data/test_images/image_0117.jpg
+../bin/recognize ../train_data/test_images/image_0119.jpg
+../bin/recognize ../train_data/test_images/image_0125.jpg
+../bin/recognize ../train_data/test_images/image_0126.jpg
