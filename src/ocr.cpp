d9c026237500849c1d9096183e5a4167442c3a29
David Pearson <dpearson26@gmail.com>
2015-03-18 00:21:54 -0400

    Improve region selection for OCR

--- src/ocr.cpp
+++ src/ocr.cpp
@@ -18,10 +18,50 @@ char *get_plate_text(Mat *img_ptr) {
 	// Dereference the image pointer
 	Mat img = *img_ptr;
 
+	Mat gray = img.clone();
+
+	// Blur the image...
+	Mat blurred(gray.cols, gray.rows, gray.type());
+	GaussianBlur(gray, blurred, Size(11, 11), 0, 0, BORDER_DEFAULT);
+
+	// Before finding edges
+	Mat edges_x(gray.rows, gray.cols, CV_8UC1);
+	Mat edges(gray.rows, gray.cols, CV_8UC1);
+
+	Sobel(gray, edges_x, CV_8U, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	threshold(edges_x, edges_x, 100, 255, CV_THRESH_BINARY);
+
+	Mat struct_elem = getStructuringElement(MORPH_RECT, Size(1, 5));
+	morphologyEx(edges_x, edges_x, MORPH_OPEN, struct_elem, Point(-1, -1), 1);
+
+	int min = img.rows;
+	int max = 0;
+	int thresh = 10;
+	for (int i = 0; i < img.rows; i++) {
+		int count = 0;
+		for (int j = 0; j < img.cols; j++) {
+			int pixel = edges_x.at<uint8_t>(i, j, 0);
+			if (pixel > 0) {
+				count++;
+			}
+		}
+
+		if (count >= thresh) {
+			if (i < min) {
+				min = i;
+			}
+
+			if (i > max) {
+				max = i;
+			}
+		}
+	}
+
+	//printf("%d, %d\n", min, max);
+
 	// Chop off the top and bottom of the image to get a better look
 	// at the license plate number
-	// TODO: FIXME
-	img = img(Rect(0, img.rows / 4, img.cols, img.rows / 2));
+	img = img(Rect(5, min, img.cols - 10, max - min));
 	/*namedWindow("plate");
 	imshow("plate", img);
 	waitKey();*/
